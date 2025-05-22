64223f479aa9727d221084f50a2bf2c0b04101e7
Matěj Straka <a@a.com>
2025-05-22 22:18:11 +0200

    upraveny zkratky lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,7 @@
 
 local lsp = require('lsp-zero')
 
+
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
@@ -24,7 +25,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-Tab>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-q>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
