7838c325cd04150a0e831f401a99b79d63d16bf0
Matěj Straka <mstraka@altech.cz>
2025-05-23 08:17:13 +0200

    git fetch je k hovnu

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>pp", "<cmd>Git push<CR>");
 vim.keymap.set("n", "<leader>ff", "<cmd>Git pull<CR>");
 -- Map <S-f> for git fetch
-vim.keymap.set('n', '<S-f>', ':Git fetch<CR>', { noremap = true, silent = true, desc = 'Git fetch with Fugitive' })
+vim.keymap.set('n', '<S-f>', ':Git pull<CR>', { noremap = true, silent = true, desc = 'Git fetch with Fugitive' })
 
 -- Map <S-r> for git rebase (e.g., rebase current branch onto upstream)
 vim.keymap.set('n', '<S-r>', ':Git rebase<CR>', { noremap = true, silent = true, desc = 'Git rebase with Fugitive' })
